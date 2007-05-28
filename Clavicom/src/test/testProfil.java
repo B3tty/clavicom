4acd138f0f20ba003bbd412fef2eccd42ab68544
thomas.devaux <thomas.devaux@2eaa401a-6b30-0410-adf4-93be5ccf8126>
2007-05-28 15:54:37 +0000

    Test de CProfil

--- Clavicom/src/test/testProfil.java
+++ Clavicom/src/test/testProfil.java
@@ -1,15 +1,9 @@
 package test;
 
-import java.io.File;
-import java.io.FileOutputStream;
-
 import org.jdom.Element;
-import org.jdom.input.SAXBuilder;
-import org.jdom.output.Format;
-import org.jdom.output.XMLOutputter;
 
 import clavicom.core.keygroup.keyboard.command.shortcutSet.CShortcutSet;
-import clavicom.core.keygroup.keyboard.key.CKeyClavicom;
+import clavicom.core.profil.CProfil;
 import clavicom.gui.language.UIString;
 
 public class testProfil
@@ -26,7 +20,15 @@ public class testProfil
 			UIString.LoadUIStringFile("Ressources\\Application\\LanguagesUI\\francais.clg");
 			CShortcutSet.CreateInstance("Ressources\\Application\\ShortcutSets\\default.css");
 			
-			String input = "Ressources\\Temp\\";
+			// Chemins
+			String input = "Ressources\\Temp\\profil.xml";
+			String output = "Ressources\\Temp\\profil_out.xml";
+			
+			// Chargement
+			CProfil myProfil = new CProfil(input);
+			
+			// Sauvegarde
+			myProfil.SaveProfil(output);
 		}
 		catch (Exception e)
 		{
