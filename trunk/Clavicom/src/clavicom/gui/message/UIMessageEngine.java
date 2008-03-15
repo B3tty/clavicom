3f73d93faad65b7b716c091a3ddd7fdb4df2e117
thomas.devaux <thomas.devaux@2eaa401a-6b30-0410-adf4-93be5ccf8126>
2008-03-15 14:57:28 +0000

    Ajout de la classe ClavicomMessageDialog

--- trunk/Clavicom/src/clavicom/gui/message/UIMessageEngine.java
+++ trunk/Clavicom/src/clavicom/gui/message/UIMessageEngine.java
@@ -25,22 +25,11 @@
 
 package clavicom.gui.message;
 
-import java.awt.BorderLayout;
-import java.awt.event.ActionEvent;
-import javax.swing.JButton;
-import javax.swing.JDialog;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.WindowConstants;
-//import java.awt.Component;
-//
-//import javax.swing.JOptionPane;
 
 import clavicom.core.listener.CMessageListener;
 import clavicom.core.message.CMessageEngine;
 import clavicom.gui.language.UIString;
-import clavicom.tools.TSwingUtils;
 
 public class UIMessageEngine implements CMessageListener
 {
@@ -60,7 +49,7 @@ public class UIMessageEngine implements CMessageListener
 	// ----------------------------------------------------------- METHODES --//
 	public void onNewFatalError(String newMessage, String detail)
 	{
-		MonDialogue d = new MonDialogue( 
+		ClavicomMessageDialog d = new ClavicomMessageDialog( 
 				parentFrame, 
 				UIString.getUIString("EX_TITLE_FATAL_ERROR"), 
 				newMessage, 
@@ -86,7 +75,7 @@ public class UIMessageEngine implements CMessageListener
 
 	public void onNewError(String newMessage, String detail)
 	{
-		MonDialogue d = new MonDialogue( 
+		ClavicomMessageDialog d = new ClavicomMessageDialog( 
 				parentFrame, 
 				UIString.getUIString("EX_TITLE_ERROR"), 
 				newMessage, 
@@ -107,7 +96,7 @@ public class UIMessageEngine implements CMessageListener
 
 	public void onNewInfo(String newMessage, String detail)
 	{
-		MonDialogue d = new MonDialogue( 
+		ClavicomMessageDialog d = new ClavicomMessageDialog( 
 				parentFrame, 
 				UIString.getUIString("EX_TITLE_INFO"), 
 				newMessage, 
@@ -126,7 +115,7 @@ public class UIMessageEngine implements CMessageListener
 
 	public void onNewWarning(String newMessage, String detail)
 	{
-		MonDialogue d = new MonDialogue( 
+		ClavicomMessageDialog d = new ClavicomMessageDialog( 
 				parentFrame, 
 				UIString.getUIString("EX_TITLE_WARNING"), 
 				newMessage, 
@@ -162,77 +151,4 @@ public class UIMessageEngine implements CMessageListener
 //			return (message + "\n\n" + detail);
 //		}
 //	}
-}
-
-class MonDialogue extends JDialog
-{
-	// les données mises à jour par la boîte de dialogue
-	String message;
-	String detail;
-
-	public MonDialogue(JFrame f, String titre, String myMessage, String myDetail, boolean modal)
-	{
-		super(f, titre, modal);
-		
-		message = myMessage;
-		detail = myDetail;
-		
-		try
-		{
-			jbInit();
-		}
-		catch (Exception e)
-		{
-			e.printStackTrace();
-		}
-	}
-
-	// les methodes de réponse aux boutons OK et Cancel
-
-	void jButtonOK_actionPerformed(ActionEvent e)
-	{
-		setVisible( false );
-	}
-
-	void jButtonCANCEL_actionPerformed(ActionEvent e)
-	{
-		setVisible( false );
-	}
-	
-	@Override
-	public void setVisible(boolean arg0)
-	{
-		// TODO Auto-generated method stub
-		TSwingUtils.centerComponentToScreen(this);
-		super.setVisible(arg0);
-	}
-
-
-	JButton jButton1 = new JButton();
-	JPanel panelButton = new JPanel();
-	JLabel lmessage = new JLabel();
-	JLabel ldetail = new JLabel();
-
-	private void jbInit() throws Exception
-	{
-		jButton1.setText("OK");
-		jButton1.addActionListener(new java.awt.event.ActionListener()
-		{
-			public void actionPerformed(ActionEvent e)
-			{
-				jButtonOK_actionPerformed(e);
-			}
-		});
-
-		lmessage.setText(message);
-		ldetail.setText(detail);
-		
-		panelButton.add(jButton1);
-		
-		this.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
-		this.setResizable(false);
-		this.getContentPane().add(panelButton, BorderLayout.SOUTH);
-		this.getContentPane().add(lmessage, BorderLayout.NORTH);
-		this.getContentPane().add(ldetail, BorderLayout.CENTER);
-	}
 }
